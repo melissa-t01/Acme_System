de239eea7b2ec84751d9d89f755d6215bbb29b10
Melissa Gutierrez <mgutierreztorres@valdosta.edu>
2024-09-02 23:22:53 -0400

    Melissa is working.

--- ACME_System/src/ver1/HelloWorld.java
+++ ACME_System/src/ver1/HelloWorld.java
@@ -3,8 +3,9 @@ package ver1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello from Eclipse");
+		System.out.println("Melissa is working locally on her branch");
 	}
 
 }
